7aa76fcbd413ca49fbaa1544190b5979fa736d6c
ashln <ashln@fruitypersonn>
2025-12-03 15:52:03 +0100

    admin

--- src/main/java/fr/insa/ashbis/webui/Application.java
+++ src/main/java/fr/insa/ashbis/webui/Application.java
@@ -2,22 +2,20 @@ package fr.insa.ashbis.webui;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
+import fr.insa.beuvron.utils.database.ConnectionPool;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-@Theme("default")
+
 public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
 
     public static void main(String[] args) {
-        // seulement si bdd h2 en memoire
-//        try (Connection con = ConnectionPool.getConnection()) {
-//            GestionSchema.razBdd(con);
-//            BdDTest.createBdDTestV2(con);
-//        } catch (SQLException ex) {
-//            throw new Error(ex);
-//        }
+        
         SpringApplication.run(Application.class, args);
     }
 
