6f72f63d4c0a94c7276200ddb619dd3af0ecdcc8
ashln <ashln@fruitypersonn>
2025-12-03 15:53:08 +0100

    admin

--- src/main/java/fr/insa/ashbis/model/Ronde.java
+++ src/main/java/fr/insa/ashbis/model/Ronde.java
@@ -17,20 +17,20 @@ import java.time.LocalDateTime;
  * @author ashln
  */
 public class Ronde extends ClasseMiroir{
-    private java.sql.Timestamp dateTimeDebut;
-    private java.sql.Timestamp dateTimeFin;
+    private java.sql.Timestamp timestampDebut;
+    private java.sql.Timestamp timestampFin;
     private int statut;
     private int idTournoi;
 
     public Ronde( int statut, int idTournoi) {
 //        java.sql.Timestamp dateTimeDebut, java.sql.Timestamp dateTimeFin,
-        this.dateTimeDebut = dateTimeDebut;
+        this.timestampDebut = null;
 //        ResultSet rs;
 //        java.sql.Timestamp test;
 //        test.toLocalDateTime().to
 //        java.sql.Date sqd = rs.getDate(1);
 //        LocalDateTime ldd = sqd.toLocalDate();
-        this.dateTimeFin = dateTimeFin;
+        this.timestampFin = null;
         this.statut = statut;
         this.idTournoi = idTournoi;
     }
@@ -38,13 +38,21 @@ public class Ronde extends ClasseMiroir{
     @Override
     protected Statement saveSansId(Connection con) throws SQLException {
         PreparedStatement pst = con.prepareStatement(
-                "insert into matchs(dateTimeDebut,statut,idTournoi,dateTimeFin) values (?,?,?,?)",
+                "insert into ronde(timestampDebut,statut,idTournoi,timestampFin) values (?,?,?,?)",
                 PreparedStatement.RETURN_GENERATED_KEYS
         );
-        pst.setTimestamp(1, this.dateTimeDebut);
+        if (this.timestampDebut == null) {
+            pst.setNull(1, java.sql.Types.TIMESTAMP);
+        } else {
+            pst.setTimestamp(1, this.timestampDebut);
+        }
         pst.setInt(2, this.statut);
         pst.setInt(3, this.idTournoi);
-        pst.setTimestamp(4, this.dateTimeFin);
+        if (this.timestampFin == null) {
+            pst.setNull(1, java.sql.Types.TIMESTAMP);
+        } else {
+            pst.setTimestamp(1, this.timestampFin);
+        }
         
         pst.executeUpdate();
         return pst;
@@ -53,29 +61,29 @@ public class Ronde extends ClasseMiroir{
     /**
      * @return the dateTimeDebut
      */
-    public java.sql.Timestamp getDateTimeDebut() {
-        return dateTimeDebut;
+    public java.sql.Timestamp getTimestampDebut() {
+        return timestampDebut;
     }
 
     /**
      * @param dateTimeDebut the dateTimeDebut to set
      */
-    public void setDateTimeDebut(java.sql.Timestamp dateTimeDebut) {
-        this.dateTimeDebut = dateTimeDebut;
+    public void setTimestampDebut(java.sql.Timestamp timestampDebut) {
+        this.timestampDebut = timestampDebut;
     }
 
     /**
      * @return the dateTimeFin
      */
-    public java.sql.Timestamp getDateTimeFin() {
-        return dateTimeFin;
+    public java.sql.Timestamp getTimestampFin() {
+        return timestampFin;
     }
 
     /**
      * @param dateTimeFin the dateTimeFin to set
      */
-    public void setDateTimeFin(java.sql.Timestamp dateTimeFin) {
-        this.dateTimeFin = dateTimeFin;
+    public void setTimestampFin(java.sql.Timestamp timestampFin) {
+        this.timestampFin = timestampFin;
     }
 
     /**
